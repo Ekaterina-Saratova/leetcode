6c00216e169309e2c406f8de446e84bc0b957921
eksa <ek.saratova@gmail.com>
2023-06-03 12:26:27 +0400

    Rename project

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     //70. Climbing Stairs
     //You are climbing a staircase. It takes n steps to reach the top.
     //Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top?
-    public class Task70
+    public class Program
     {
         static void Main(string[] args)
         {
