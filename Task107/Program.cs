ce5b2f109a4546f1234ba5828497fee4c136c4bb
Ekaterina-Saratova <44108145+Ekaterina-Saratova@users.noreply.github.com>
2023-07-05 20:14:28 +0400

    +task107, +task28, +task20

---
+++ Task107/Program.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tas107
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var task107 = new Solution();
+            var tree1 = new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            Assert.That(task107.MaxDepth(tree1), Is.EqualTo(3));
+            Console.ReadKey();
+        }
+    }
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            var leftDepth = MaxDepth(root.left);
+            var rightDepth = MaxDepth(root.right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+
+        //Обход в глубину.
+        public void DepthTraverse(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(node.val);
+            DepthTraverse(node.left);
+            DepthTraverse(node.right);
+        }
+
+        //Обход в ширину
+        void WidthTraversal(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                Console.WriteLine(current.val);
+                if (current.left != null)
+                    queue.Enqueue(current.left);
+                if (current.right != null)
+                    queue.Enqueue(current.right);
+            }
+        }
+    }
+
+    //Плохое решение, но пригодится для получения списка элементов одного уровня.
+    //public int MaxDepth(TreeNode root)
+    //{
+    //    if (root == null)
+    //        return 0;
+    //    var counter = 1;
+    //    var list = new List<TreeNode>();
+    //    if (root.left != null)
+    //        list.Add(root.left);
+    //    if (root.right != null)
+    //        list.Add(root.right);
+    //    if (list.Count > 0)
+    //        Traverse(list, ref counter);
+    //    return counter;
+    //}
+
+    //public void Traverse(List<TreeNode> nodeList, ref int counter)
+    //{
+    //    counter++;
+    //    var nextLevelList = new List<TreeNode>();
+    //    foreach (var l in nodeList)
+    //    {
+    //        if (l.left != null)
+    //            nextLevelList.Add(l.left);
+    //        if (l.right != null)
+    //            nextLevelList.Add(l.right);
+    //    }
+    //    if (nextLevelList.Count > 0)
+    //        Traverse(nextLevelList, ref counter);
+    //}
+
+    //Подсчет элементов.
+    //public int MaxDepth(TreeNode root)
+    //{
+    //    var counter = 0;
+    //    Traverse(root, ref counter);
+    //    return counter;
+    //}
+
+    //public void Traverse(TreeNode node, ref int counter)
+    //{
+    //    if (node == null) return;
+    //    Traverse(node.left, ref counter);
+    //    Traverse(node.right, ref counter);
+    //    counter++;
+    //}
+}
---
+++ Task28/Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace Task28
+{
+    /// <summary>
+    /// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+    /// or -1 if needle is not part of haystack.
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var task28 = new Solution();
+            Assert.That(task28.StrStr("sadbutsad", "sad"), Is.EqualTo(0));
+            Assert.That(task28.StrStr("leetcode", "leeto"), Is.EqualTo(-1));
+            Assert.That(task28.StrStr("mississippi", "issip"), Is.EqualTo(4));
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length > haystack.Length) return -1;
+            for (var i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    var index = i;
+                    for (var j = 0; j < needle.Length && i < haystack.Length; j++)
+                    {
+                        if (haystack[i] != needle[j]) break;
+                        if (j == needle.Length - 1) return index;
+                        i++;
+                    }
+
+                    i = index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
