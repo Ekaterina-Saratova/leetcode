8f9c4a6a664dd560e19cbb13b04561fcee62407b
Ekaterina-Saratova <44108145+Ekaterina-Saratova@users.noreply.github.com>
2024-01-01 16:52:33 +0400

    gring75 1 brute force

---
+++ 100-200/1.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Grind75
+{
+    public class Solution1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var i = 0;
+            while (i < nums.Length)
+            {
+                var j = i+1;
+                while (j < nums.Length)
+                {
+                    if (nums[j] + nums[i] == target)
+                        return new[] { i, j };
+                    j++;
+                }
+
+                i++;
+            }
+
+            return new int[2];
+        }
+    }
+
+    [TestFixture]
+    public class Tests1
+    {
+        [TestCase(new [] { 2, 7, 11, 15 }, 9, new [] { 0, 1 })]
+        [TestCase(new [] { 3, 2, 4 }, 6, new [] { 1,2 })]
+        [TestCase(new [] { 3, 3 }, 6, new [] { 0,1 })]
+        public void Check(int[] nums, int target, int[] expected)
+        {
+            var actual = new Solution1().TwoSum(nums, target);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
