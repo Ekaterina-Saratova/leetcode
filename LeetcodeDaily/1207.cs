18b425e7bdb88c85f823aeda82c744512f0f5bf2
Ekaterina-Saratova <44108145+Ekaterina-Saratova@users.noreply.github.com>
2024-01-17 22:05:40 +0400

    leetcode daily 1207

---
+++ LeetcodeDaily/1207.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeDaily
+{
+    public class Solution1207
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var occurrences = new Dictionary<int, int>();
+            foreach (var item in arr)
+            {
+                if (occurrences.ContainsKey(item))
+                    occurrences[item]++;
+                else
+                    occurrences.Add(item, 1);
+            }
+
+            var unique = new HashSet<int>();
+            foreach (var item in occurrences)
+            {
+                if (unique.Contains(item.Value))
+                    return false;
+                unique.Add(item.Value);
+            }
+
+            return true;
+        }
+    }
+
+    [TestFixture]
+    public class Tests1207
+    {
+        [TestCase(new int[] { 1, 2, 2, 1, 1, 3 }, true)]
+        [TestCase(new int[] { 1, 2 }, false)]
+        [TestCase(new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 }, true)]
+        public void Test(int[] arr, bool expected)
+        {
+            var result = new Solution1207().UniqueOccurrences(arr);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
