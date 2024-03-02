ec15c65db92cf80893c3c63375442c1f1e96c122
Ekaterina-Saratova <ek.saratova@gmail.com>
2024-03-02 12:25:47 +0300

    daily 977

---
+++ LeetcodeDaily/977.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Collections;
+
+namespace LeetCodeDaily
+{
+    public class Solution977
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            var squares = nums.Select(n => n * n).ToArray();
+            Array.Sort(squares);
+            return squares;
+        }
+    }
+}
