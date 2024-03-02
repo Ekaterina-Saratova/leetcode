17832bd84f61e6c5181ec0cb1c582a6725d2c78d
Ekaterina-Saratova <ek.saratova@gmail.com>
2024-03-02 12:26:01 +0300

    daily 977 tests

--- LeetcodeDaily/977.cs
+++ LeetcodeDaily/977.cs
@@ -12,4 +12,16 @@ namespace LeetCodeDaily
             return squares;
         }
     }
+
+    [TestFixture]
+    public class Tests977
+    {
+        [TestCase(new[] { -4, -1, 0, 3, 10 }, new[] { 0, 1, 9, 16, 100 })]
+        [TestCase(new[] { -7, -3, 2, 3, 11 }, new[] { 4, 9, 9, 49, 121 })]
+        public void Test(int[] nums, int[] expected)
+        {
+            var result = new Solution977().SortedSquares(nums);
+            Assert.AreEqual(expected, result);
+        }
+    }
 }
