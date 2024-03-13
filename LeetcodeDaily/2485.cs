e7041f39eb992dd10d52b1cab8a6edb37afcb7d0
Ekaterina-Saratova <ek.saratova@gmail.com>
2024-03-13 20:07:07 +0300

    2485 daily

---
+++ LeetcodeDaily/2485.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeDaily
+{
+    public class Solution2485
+    {
+        public int PivotInteger(int n)
+        {
+            for (double i = 1; i <= n; i++)
+            {
+                var left = (i / 2) * (1 + i);
+                var rigth = ((n - i + 1) / 2) * (i + n);
+                if (left == rigth)
+                    return (int)i;
+            }
+            return -1;
+        }
+    }
+
+    [TestFixture]
+    public class Tests2485
+    {
+        [Test]
+        [TestCase(8, 6)]
+        public void Test(int n, int expected)
+        {
+            var result = new Solution2485().PivotInteger(n);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
