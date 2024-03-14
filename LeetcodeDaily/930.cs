85bc81bb9ee9e1a45f4064f8c61e6c127501ae8d
Ekaterina-Saratova <ek.saratova@gmail.com>
2024-03-14 18:28:28 +0300

    daily 930 primitive

---
+++ LeetcodeDaily/930.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeDaily
+{
+    public class Solution930
+    {
+        public int NumSubarraysWithSum(int[] nums, int goal)
+        {
+            var last = 0;
+            var sum = 0;
+            var result = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (sum == goal)
+                {
+                    result++;
+                }
+                if (sum > goal || i == nums.Length-1)
+                {
+                    sum = 0;
+                    i = last;
+                    last++;
+                }
+            }
+            return result;
+        }
+    }
+
+    [TestFixture]
+    public class Tests930
+    {
+        [Test]
+        [TestCase(new int[]{ 1, 0, 1, 0, 1 },2, 4)]
+        public void Test(int[] n, int goal, int expected)
+        {
+            var result = new Solution930().NumSubarraysWithSum(n, goal);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
