57768721c29c55482267564507b2c7024633a54b
Ekaterina-Saratova <ek.saratova@gmail.com>
2024-08-13 19:20:16 +0400

    before cleaning2

---
+++ LeetcodeDaily/633.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeDaily
+{
+    public class Solution633
+    {
+        public bool JudgeSquareSum(int c)
+        {
+            long a = 0;
+            long b = (long)Math.Ceiling(Math.Sqrt(c));
+            var max = b;
+
+            while (a <= max && b >= 0)
+            {
+                var c1 = (a * a + b * b);
+                if (c1 == c)
+                    return true;
+                if (c1 < c)
+                    a++;
+                if (c1 > c)
+                    b--;
+            }
+
+            return false;
+        }
+    }
+
+    [TestFixture]
+    public class Tests633
+    {
+        [TestCase(8, true)]
+        public void Test(int c, bool expected)
+        {
+            var result = new Solution633().JudgeSquareSum(c);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
