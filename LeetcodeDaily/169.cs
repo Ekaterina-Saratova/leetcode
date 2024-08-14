196598339ac88454ed2fa2db0275bc9895ffed52
Ekaterina-Saratova <44108145+Ekaterina-Saratova@users.noreply.github.com>
2024-08-14 19:48:39 +0400

    129

---
+++ LeetcodeDaily/169.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeDaily
+{
+    public class Solution169
+    {
+        //Should use Boyer-Moore Voting Algorithm.
+        public int MajorityElement(int[] nums)
+        {
+            double n = nums.Length / 2;
+            var dict = new Dictionary<int, int>();
+            var max = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (dict.TryAdd(nums[i], 1))
+                {
+                    if (dict.Count == 1)
+                        max = nums[i];
+                }
+                else
+                {
+                    var newMax = ++dict[nums[i]];
+                    if (dict[max] < newMax)
+                        max = nums[i];
+                }
+            }
+
+            return max;
+        }
+    }
+
+    [TestFixture]
+    public class Test169
+    {
+        [Test]
+        [TestCase(new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2)]
+        [TestCase(new int[] { 3, 2, 3 }, 3)]
+        [TestCase(new int[] { 6, 5, 5 }, 5)]
+        public void Test(int[] arr, int expected)
+        {
+            var solution = new Solution169();
+            Assert.AreEqual(expected, solution.MajorityElement(arr));
+        }
+    }
+}
