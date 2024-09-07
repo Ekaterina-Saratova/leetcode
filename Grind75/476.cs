650b820979a4600a283726d9ff556760359c400b
Ekaterina-Saratova <ek.saratova@gmail.com>
2024-09-07 16:08:55 +0400

    476

---
+++ Grind75/476.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace Grind75
+{
+    public class Solution476
+    {
+        //Should use bit mask instead.
+        public int FindComplement(int num)
+        {
+            var s = Convert.ToString(num, 2);
+            var newNum = new StringBuilder(s.Length);
+            foreach (var ch in s)
+            {
+                newNum.Append(ch == '1' ? '0' : '1');
+            }
+            return Convert.ToInt32(newNum.ToString(), 2);
+        }
+    }
+
+    [TestFixture]
+    public class Tests476
+    {
+        [TestCase( 5, 2)]
+        [TestCase( 1, 0)]
+        [TestCase( 2, 1)]
+        public void Check(int target, int expected)
+        {
+            var actual = new Solution476().FindComplement(target);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
