e0f2edc69e8aaf3876e23723e6b180dc6c4f54d8
Ekaterina-Saratova <44108145+Ekaterina-Saratova@users.noreply.github.com>
2024-12-17 20:30:43 +0400

    26

---
+++ LeetcodeDaily/26.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeDaily
+{
+    public class Solution26
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            for (var i = 0; i < haystack.Length; i++)
+            {
+                var index = 0;
+                while (index < needle.Length && index + i < haystack.Length && haystack[i + 1] == needle[index])
+                {
+                    index++;
+                }
+                if (index == needle.Length)
+                    return i;
+            }
+            return -1;
+        }
+    }
+
+    [TestFixture]
+    public class Test26
+    {
+        [Test]
+        [TestCase("sadbutsad", "sad", 2)]
+        public void Test(string haystack, string needle, int expectedCount)
+        {
+            var solution = new Solution26();
+            var actual = solution.StrStr(haystack, needle);
+            Assert.AreEqual(expectedCount, actual);
+        }
+    }
+}
